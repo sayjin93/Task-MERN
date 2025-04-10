de6a411b9938652ad99105a3474060eb71016d7f
Jurgen Kruja <jurgen-kruja@live.com>
2025-04-11 01:52:39 +0200

    Initialize Express server with dotenv configuration

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,7 @@
-console.log("Server is running...");
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = 8000;
+const app = express();
+
+app.listen(port, () => console.log(`Server listening on ${port}`));
+
