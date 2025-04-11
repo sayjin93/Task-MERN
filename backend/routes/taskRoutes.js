a8fecb8dd17b2cafb49a3783dd505df0d54fa994
Jurgen Kruja <jurgen-kruja@live.com>
2025-04-11 21:32:56 +0200

    Add CRUD routes for tasks in taskRoutes.js

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,8 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.status(200).json({ message: 'Get All Tasks' });
+router.get("/", (req, res) => {
+  res.status(200).json({ message: "Get All Tasks" });
+});
+
+router.post("/", (req, res) => {
+  res.status(200).json({ message: "Create Task" });
+});
+
+router.put("/:id", (req, res) => {
+  res.status(200).json({ message: `Task ${req.params.id} updated.` });
+});
+
+router.delete("/:id", (req, res) => {
+  res.status(200).json({ message: `Task ${req.params.id} deleted.` });
 });
 
 module.exports = router;
