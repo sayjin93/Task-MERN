c80590e7adfc16f9ff047a7e9688fdde2cc443df
Jurgen Kruja <jurgen-kruja@live.com>
2025-04-11 15:26:20 +0200

    Page 43 completed

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
-const port = 8000;
+const port = process.env.PORT || 8000;
 const app = express();
+
+app.use('/api/tasks', require('./routes/taskRoutes'));
 
 app.listen(port, () => console.log(`Server listening on ${port}`));
 
