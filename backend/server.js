77ba8e481074cabc9b8d6cd8c94f2e42b3517c72
Jurgen Kruja <jurgen-kruja@live.com>
2025-05-09 02:46:38 +0200

    Completing chapter 4

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/tasks', require('./routes/taskRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
 
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server listening on ${port}`));
-
