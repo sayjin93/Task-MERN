28c97cd9307634b65b43f1f2aae6dd1a4924bf9a
Jurgen Kruja <jurgen-kruja@live.com>
2025-05-30 00:09:46 +0200

    Deleting task

--- frontend/src/components/TaskItem.jsx
+++ frontend/src/components/TaskItem.jsx
@@ -1,10 +1,24 @@
-import { useDispatch } from 'react-redux';
+import { useDeleteTaskMutation } from '../store/apis/taskApi';
+import { toast } from 'react-toastify';
 
 const TaskItem = ({ task }) => {
+    const [deleteTask] = useDeleteTaskMutation();
+
+    const handleDelete = async () => {
+        const result = await deleteTask(task._id);
+
+        if (result.error) {
+            toast.error(result.error.data?.message || 'Fshirja dështoi!');
+        } else {
+            toast.success('Detyra u fshi me sukses!');
+        }
+    };
+
     return (
         <div className='task'>
             <div>{new Date(task.createdAt).toLocaleString('sq-AL')}</div>
             <h2>{task.text}</h2>
+            <button onClick={handleDelete} className='close'>X</button>
         </div>
     );
 };
