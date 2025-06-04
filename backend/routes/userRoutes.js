5b6b804320f8d6049d0658538401025dfdb8d37a
Jurgen Kruja <jurgen-kruja@live.com>
2025-06-04 09:27:10 +0200

    Fixes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,8 +3,6 @@ const router = express.Router();
 
 const { registerUser, loginUser } = require('../controllers/userController');
 
-const { protect } = require('../middleware/authMiddleware');
-
 router.post('/', registerUser);
 router.post('/login', loginUser);
 
