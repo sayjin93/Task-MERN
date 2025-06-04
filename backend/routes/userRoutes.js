f932715936d33963c26715cbe6332115eb5ccf3a
Jurgen Kruja <jurgen-kruja@live.com>
2025-06-04 09:21:48 +0200

    Cleanup user api

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { registerUser, loginUser, getCurrentUser } = require('../controllers/userController');
+const { registerUser, loginUser } = require('../controllers/userController');
 
 const { protect } = require('../middleware/authMiddleware');
 
 router.post('/', registerUser);
 router.post('/login', loginUser);
-router.get('/current', protect, getCurrentUser);
 
 module.exports = router;
